18a8f57360cc535351a8cdff163e64dd723a47dc
SantiCHP <santichadmin@gmail.com>
2021-06-22 09:44:17 +0100

    Add files via upload

---
+++ dist/app.dev.js
@@ -0,0 +1,58 @@
+"use strict";
+
+var ubicaciones = [{
+  localizacion: 'Gran Canaria. Las Palmas de Gran Canaria',
+  posicion: [28.1280511, -15.44028138888889]
+}, {
+  localizacion: 'Tenerife. Santa Cruz de Tenerife',
+  posicion: [28.463122, -16257559]
+}, {
+  localizacion: 'Fuerteventura. Puerto del Rosario',
+  posicion: [28.500586921985388, -13.859574297656096]
+}, {
+  localizacion: 'Praia, Cape Verde',
+  posicion: [14.916733, -23.509345]
+}, {
+  localizacion: 'Dakar, Senegal',
+  posicion: [14.695589, -17.472145]
+}];
+newGeoloc = [].concat(ubicaciones); // Dado un array de objetos con localizaciones del planeta.
+//     1- Geolocalizar por latitud y longitud cada una de ellas para completar el array (donde se encuentran: lat y long)
+// HECHO
+//     2- Crear una función que recupere datos de la lista mediante el método del callback (simular 2 segundos de retardo por el contacto con el servidor)
+
+var map;
+var lat = -34.397;
+var lng = 150.644;
+document.querySelector('#exampleSelect1').addEventListener('submit', function (e) {
+  //Prevent actual submit
+  e.preventDefault();
+
+  function solicitaDatos(item) {
+    setTimeout(function () {
+      // Función temporizadora
+      newGeoloc.forEach(function (geo) {
+        if (geo.localizacion.includes(item)) {
+          var initMap = function initMap() {
+            map = new google.maps.Map(document.getElementById("map"), {
+              center: {
+                lat: _lat,
+                lng: _lng
+              },
+              zoom: 8
+            });
+          };
+
+          var _lat = geo.posicion[0];
+          var _lng = geo.position[1];
+        } else {
+          console.log('No existe la localización indicada');
+        }
+      });
+    }, 1000); // Simulamos que tarda un tiempo entre la solicitud y la llegada de la información
+  }
+}); //https://developers.google.com/maps/documentation/javascript/examples/map-simple#maps_map_simple-html <-- indicaciones para googleAPI MAPS
+//     3- Crear otra función que inserte datos de otras ciudades en la lista y muestre a continuación la lista completa (simular 3 segundos de retardo)
+//     4- Repetir los ejercicios 2 y 3 creando una promesa. Tener en cuenta la gestión de errores.
+//     5- Crear una promesa que solicite una localización y la elimine del array. 
+//     6- Por último, crear una promesa que modifique la localización de alguna de las ciudades incluidas en la fuente de datos.
